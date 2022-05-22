49b7f64430fd9c0cb075e2abc30a89d12186db9e
Viktoriia Yehorova <egorova.vik@gmail.com>
2022-05-22 20:26:44 +0300

    Added CsvHeader class

--- paypal/domain/csv_logic/util.py
+++ paypal/domain/csv_logic/util.py
@@ -1,49 +1,63 @@
-""" CSV headers. """
+class CsvHeaders:
+    """ CSV headers. """
 
-paypal_account_headers = [
-    "id",
-    "account_type",
-    "balance"
-]
+    paypal_account_headers = [
+        "id",
+        "account_type",
+        "balance"
+    ]
 
-account_personal_data_headers = [
-    "account",
-    "full_name",
-    "date_of_birth",
-    "country",
-    "phone_number",
-    "avatar",
-    "password",
-    "email"
-]
+    account_personal_data_headers = [
+        "account",
+        "full_name",
+        "date_of_birth",
+        "country",
+        "phone_number",
+        "avatar",
+        "password",
+        "email"
+    ]
 
-billing_address_headers = [
-    "id",
-    "account_personal_data",
-    "street_address",
-    "additional_information",
-    "center_of_population",
-    "region",
-    "zip_code"
-]
+    billing_address_headers = [
+        "id",
+        "account_personal_data",
+        "street_address",
+        "additional_information",
+        "center_of_population",
+        "region",
+        "zip_code"
+    ]
 
-card_headers = [
-    "id",
-    "account",
-    "billing_address",
-    "balance",
-    "is_preferred",
-    "card_number",
-    "cvv",
-    "expiration_date"
-]
+    card_headers = [
+        "id",
+        "account",
+        "billing_address",
+        "balance",
+        "is_preferred",
+        "card_number",
+        "cvv",
+        "expiration_date"
+    ]
 
-transaction_headers = [
-    "id",
-    "from_card",
-    "to_card",
-    "finished_at",
-    "type",
-    "payment_method",
-    "status"
-]
+    transaction_headers = [
+        "id",
+        "from_card",
+        "to_card",
+        "finished_at",
+        "type",
+        "payment_method",
+        "status"
+    ]
+
+    @classmethod
+    def get_headers(cls) -> list:
+        """
+        Return all headers as list.
+        """
+        return [
+            CsvHeaders.paypal_account_headers,
+            CsvHeaders.account_personal_data_headers,
+            CsvHeaders.billing_address_headers,
+            CsvHeaders.card_headers,
+            CsvHeaders.transaction_headers
+        ]
