58e5610aff2a2a26691de9e9e72bbf2ac00a86ab
Viktoriia Yehorova <egorova.vik@gmail.com>
2022-05-22 20:30:53 +0300

    Moved util logic to util classes, implemented handler class injection

--- paypal/domain/csv_logic/csv_reader.py
+++ paypal/domain/csv_logic/csv_reader.py
@@ -2,53 +2,29 @@ import csv
 import uuid
 from typing import Union
 
-from paypal.domain.csv_logic.util import (
-    paypal_account_headers,
-    account_personal_data_headers,
-    billing_address_headers,
-    card_headers,
-    transaction_headers,
-)
+from injector import inject
+
+from paypal.domain.core.util import EntityVerbose
+from paypal.domain.csv_logic.util import CsvHeaders
 
 
-class CsvReader:
-    """ CSV file parser. """
+class CsvConverterHandler:
+    """ CSV data converter. """
 
     @classmethod
-    def _is_row_a_header(cls, row: list) -> bool:
-        """
-        Check if current row is a header.
-        """
-        return row in [
-            paypal_account_headers,
-            account_personal_data_headers,
-            billing_address_headers,
-            card_headers,
-            transaction_headers
-        ]
-
-    @classmethod
-    def _is_row_blank(cls, row: list) -> bool:
-        """
-        Check if current row is blank.
-        """
-        return row[0] == '"'
-
-    @classmethod
-    def _map_header_to_entity_name(cls, header: list) -> str:
+    def map_header_to_entity_name(cls, header: list) -> str:
         """
         Return entity verbose name based on headers.
         """
+        headers = CsvHeaders().get_headers()
+        entity_names = EntityVerbose().get_verbose_names()
         return {
-            tuple(paypal_account_headers): 'PayPal Account',
-            tuple(account_personal_data_headers): 'Account Personal Data',
-            tuple(billing_address_headers): 'Billing Address',
-            tuple(card_headers): 'Card',
-            tuple(transaction_headers): 'Transaction'
+            tuple(headers[i]): entity_names[i]
+            for i in range(len(headers))
         }.get(tuple(header), None)
 
     @classmethod
-    def _convert_str_to_uuid(cls, value: Union[str, int]) -> Union[str, uuid.UUID]:
+    def convert_str_to_uuid(cls, value: Union[str, int]) -> Union[str, uuid.UUID]:
         """
         Convert string UUIDs to UUID objects.
         """
@@ -63,7 +39,7 @@ class CsvReader:
         return value
 
     @classmethod
-    def _convert_row_to_dict(cls, header: list, row: list) -> dict:
+    def convert_row_to_dict(cls, header: list, row: list) -> dict:
         """
         Convert current row to a dict of column names and values.
         """
@@ -72,17 +48,37 @@ class CsvReader:
             for j in range(len(row))
         }
 
+
+class CsvReader:
+    """ CSV file parser. """
+
+    @inject
+    def __init__(self, csv_converter: CsvConverterHandler = CsvConverterHandler()):
+        self.csv_converter = csv_converter
+        super().__init__()
+
     @classmethod
-    def parse(cls, filename: str = 'generated.csv') -> dict:
+    def _is_row_a_header(cls, row: list) -> bool:
+        """
+        Check if current row is a header.
+        """
+        return row in CsvHeaders().get_headers()
+
+    @classmethod
+    def _is_row_blank(cls, row: list) -> bool:
+        """
+        Check if current row is blank.
+        """
+        return row[0] == '"'
+
+    def parse(self, filename: str = 'generated.csv') -> dict:
         """
         Read CSV file and return a dictionary of rows related to specific models.
         """
+        entity_names = EntityVerbose.get_verbose_names()
         result = {
-            'PayPal Account': [],
-            'Account Personal Data': [],
-            'Billing Address': [],
-            'Card': [],
-            'Transaction': []
+            entity_names[i]: []
+            for i in range(len(entity_names))
         }
         with open(f'../../{filename}', newline='\n') as csvfile:
             reader = csv.reader(csvfile, delimiter=';', quotechar='|')
@@ -96,15 +92,15 @@ class CsvReader:
                         print(f'Found {current_entity_counter} entities of {current_entity}.')
                         current_entity_counter = 0
 
-                    current_entity = CsvReader._map_header_to_entity_name(row)
+                    current_entity = self.csv_converter.map_header_to_entity_name(row)
                     current_header = row
                     print(f'Reading entities of class: {current_entity}...')
                 elif not CsvReader._is_row_blank(row):
                     for i in range(len(row)):
-                        row[i] = CsvReader._convert_str_to_uuid(row[i])
+                        row[i] = self.csv_converter.convert_str_to_uuid(row[i])
 
                     result[current_entity].append(
-                        CsvReader._convert_row_to_dict(current_header, row)
+                        self.csv_converter.convert_row_to_dict(current_header, row)
                     )
 
                     current_entity_counter += 1
